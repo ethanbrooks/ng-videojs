cea530d2937235ebb3e468e1b00da9091868157a
ethan brooks <ethanbrooks@ethans-MacBook-Air.local>
2019-06-12 14:06:55 -0500

    overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { HubConnection } from '@aspnet/signalr';
 import * as signalR from '@aspnet/signalr';
 
 import videojs from 'video.js';
-
+import 'videojs-overlay';
 
 
 @Component({
@@ -16,25 +16,75 @@ import videojs from 'video.js';
 mp4hls --encryption-key baab6d0dd153762d945d5a060abb5fcd --output-encryption-key ~/Downloads/BigBuckBunny_320x180.mp4
 */
 export class AppComponent implements AfterViewInit {
-  @ViewChild('myvid') vid: ElementRef;
+  @ViewChild('myvid') vid: ElementRef<any>;
   private _hubConnection: HubConnection | undefined;
   public async: any;
 
 
   poster = 'https://s3.amazonaws.com/my.safepics.com/banner0.jpg';
   
+  fullScreen() {
+    let mustClickElem = document.getElementById('must-click');
+    let videoContentElem = document.getElementById('videocontent');
+    mustClickElem.style.display = 'none';
+    videoContentElem.style.display = 'block';
+
+    let methodToBeInvoked = mustClickElem.requestFullscreen 
+      //||mustClickElem.webkitRequestFullScreen 
+      || mustClickElem['mozRequestFullscreen']
+      ||mustClickElem['msRequestFullscreen'];
+    if (methodToBeInvoked) methodToBeInvoked.call(mustClickElem);
+  }
+
   ngAfterViewInit() {
     const options = {
-      controls: false,
+      controls: true,
       fluid: true,
-      muted: true,
       preload: 'auto',
       techOrder: ['html5'],
    };
 
     let player = new videojs(this.vid.nativeElement, options, function onPlayerReady() {
-    //      this.enterFullScreen();
-          console.log('Your player is ready!');
+
+    player.overlay({
+      content: 'Default overlay content',
+      debug: true,
+      overlays: [{
+        content: 'The video is playing!',
+        start: 'play',
+        end: 'pause'
+      }, {
+        start: 0,
+        end: 15,
+        align: 'bottom-left'
+      }, {
+        start: 15,
+        end: 30,
+        align: 'bottom'
+      }, {
+        start: 30,
+        end: 45,
+        align: 'bottom-right'
+      }, {
+        start: 20,
+        end: 'pause'
+      }]
+    });
+
+     player.on('pause', function()
+      {
+        console.log('show');
+        document.getElementById('videocontent').innerHTML = '<div style="color:white"><h1>FUCK YOU</h1></div>';
+      });
+    
+      player.on('play', function()
+      {
+        console.log('hide');
+        this.posterImage.hide();
+      });
+
+
+      console.log('Your player is ready!');
           // How about an event listener?
           this.on('ended', function() {
             console.log('Awww...over so soon?!');
@@ -59,15 +109,15 @@ export class AppComponent implements AfterViewInit {
         if(count == 3){
           console.log('PLAYING');
           player.src([
-//            {type: "video/mp4", src:"https://s3.amazonaws.com/hh-video/LogoFull_1.mp4"}
-            {type:"application/x-mpegURL", src:"/assets/videos/14-01-17-103626-27/stream.m3u8"}
+//            {type: "video/mp4", src:"/assets/videos/14-01-17-103626-24.mp4"}
+{type: "video/mp4", src:"https://hotel-herrera.s3.amazonaws.com/assets/video/mp4/HERRERA_1.mp4"}
           ]);
           var promise = player.play();    
           if (promise !== undefined) {
             promise.then(function() {
               console.log('Autoplay started!');
-//              document.getElementById('videocontent').style.display = 'block';
-//              document.getElementById('must-click').style.display = 'none';              
+              document.getElementById('videocontent').style.display = 'block';
+              document.getElementById('must-click').style.display = 'none';              
             }).catch(function(error) {
 
               var d = new Date();
